54fdac05bc0f3da4635f13a94bf09f355129bbd4
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-11 20:35:25 +0300

    Update Task3.cpp

--- Task3.cpp
+++ Task3.cpp
@@ -1,41 +1,38 @@
 /*
-2.	Элементы массива размерностью  2 на 6 инициализировать 
-случайными числами от 0 до 5. Для каждой 
-строки распечатать индекс первого нулевого элемента (или слово “нет”).
+3.Элементы квадратной матрицы  5 на 5 
+инициализировать случайными числами 
+от 0 до 10. Найти сумму максимальных 
+элементов всех столбцов.
 */
 
 #include <iostream>
 #include <ctime>
-#define ROW 2
-#define COLUMN 6
+#define SIZE 5
+
 using namespace std;
 void main() {
 	setlocale(LC_ALL, "rus");
 	srand(time(0));
-	int ourArr[ROW][COLUMN];
+	int ourArr[SIZE][SIZE];
+	int sum = 0;
 
-	for (int i = 0; i < ROW; i++)
-		for (int j = 0; j < COLUMN; j++)
-			ourArr[i][j] = rand() % 5;
-	for (int i = 0; i < ROW; i++) {
-		for (int j = 0; j < COLUMN; j++)
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < SIZE; j++)
+			ourArr[i][j] = rand() % 10;
+
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++)
 			cout << ourArr[i][j]<< " ";
 		cout << endl;
 	}
-
-	cout << endl;
-
-	for (int i = 0; i < ROW; i++) {
-		int columnIndex = -1;
-		for (int j = 0; j < COLUMN; j++)
-			if (ourArr[i][j] == 0) {
-				columnIndex = j;
-				break;
-			}
-		if (columnIndex == -1) cout << "нет";
-		else cout << columnIndex; 
-		cout << endl;
+	cout << "\nМаксимальные элементы столбцов:\n";
+	for (int i = 0; i < SIZE; i++) {
+		int max = ourArr[0][i];
+		for (int j = 1; j < SIZE; j++)
+			if (ourArr[j][i] > max) max = ourArr[j][i];
+		cout << max << " ";
+		sum += max;		
 	}
-	
+	cout << "\n\nСумма максимальных элементов всех столбцов " << sum << endl;	
 	system("pause");
 }
