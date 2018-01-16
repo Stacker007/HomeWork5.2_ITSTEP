e3c1f8ad4599aa4710e1b274ee385ecbb8f89108
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-16 18:48:38 +0300

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,7 +1,7 @@
 /*
-1.	Элементы двумерного массива размерностью 3 на 4 
-инициализировать случайными числами от 10 до 30. 
-Вывести исходный массив. Затем обнулить вторую 
+1.	Элементы двумерного массива размерностью 3 на 4
+инициализировать случайными числами от 10 до 30.
+Вывести исходный массив. Затем обнулить вторую
 строку и вывести измененный массив.
 srand(time(0));
 int my=rand() % 500 +1;
@@ -10,19 +10,30 @@ int my=rand() % 500 +1;
 #include <iostream>
 #include <ctime>
 #define ROW 4
-#define COLUMN 3
+#define COL 3
 using namespace std;
+
+}
 void main() {
 	setlocale(LC_ALL, "rus");
 	srand(time(0));
-	int ourArr[COLUMN][ROW];
+	int ourArr[ROW][COL];
 	for (int i = 0; i < ROW; i++)
-		for (int j = 0; j < COLUMN; j++)
-			ourArr[i][j]= rand() % 20 + 10;
-	for (int i = 0; i < ROW; i++){
-		for (int j = 0; j < COLUMN; j++)
-			cout << ourArr[i][j]<< " " ;
+		for (int j = 0; j < COL; j++)
+			ourArr[i][j] = rand() % 21 + 10;
+	for (int i = 0; i < ROW; i++) {
+		for (int j = 0; j < COL; j++)
+			cout << ourArr[i][j] << " ";
 		cout << endl;
-		}
+	}
+	cout << endl;
+	
+	for (int j = 0; j < COL; j++)
+		ourArr[2][j] = 0;
+	for (int i = 0; i < ROW; i++) {
+		for (int j = 0; j < COL; j++)
+			cout << ourArr[i][j] << " ";
+		cout << endl;
+	}
 	system("pause");
 }
